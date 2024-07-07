da6b649e2012e73e203632c2cd0cdfbe08fca437
NathanOnCodes <contatonathancf@gmail.com>
2024-07-07 19:34:36 -0300

    feat: start

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rescisao_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rescisao_facil_projeto.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- rescisao_facil_projeto/wsgi.py
+++ rescisao_facil_projeto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rescisao_api project.
+WSGI config for rescisao_facil_projeto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rescisao_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rescisao_facil_projeto.settings')
 
 application = get_wsgi_application()
