996e87153a6535ccb273f75bba0149b6b4d295f3
NathanOnCodes <contatonathancf@gmail.com>
2024-07-19 23:21:12 -0300

    refactor: refatorando rotas da versao 1

--- rescisao_facil_projeto/urls.py
+++ rescisao_facil_projeto/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/v1/', include('rescisao.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/rescisao', include('rescisao.urls')),
 ]
