e2c78b2d8be0cb55a97988fa6525d61a2e4f0565
NathanOnCodes <66134107+NathanOnCodes@users.noreply.github.com>
2024-09-22 18:57:02 -0300

    Delete rescisao

--- rescisao/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Contrato, Funcionario
-
-# Register your models here.
-admin.site.register(Contrato)
-admin.site.register(Funcionario)
--- rescisao/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RescisaoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rescisao'
