427c839bebbdde1030add70d5ecd95668007f4af
NathanOnCodes <66134107+NathanOnCodes@users.noreply.github.com>
2024-09-22 19:39:32 -0300

    Delete rescisao_api

--- rescisao_api/api.py
+++
@@ -1,6 +0,0 @@
-from ninja import NinjaAPI
-from rescisao.api import router as rescisao_router
-
-api = NinjaAPI(title="Rescisão API", version="0.0.1")
-
-api.add_router("/rescisao/", rescisao_router)
